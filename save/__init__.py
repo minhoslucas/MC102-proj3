9ec6c129a7093d5e48cfa543bd52da0ec1bc0743
AmeMeida <ame.pistache@gmail.com>
2024-07-04 22:43:31 -0300

    preparativos pra save prontos

--- save/__init__.py
+++ save/__init__.py
@@ -1,3 +1,34 @@
+from dataclasses import dataclass
+from maze import MazeTemplate, mazes
 
-class Save:
-    pass    
+from save.reader import read, write
+
+@dataclass
+class SaveData:
+    maze: MazeTemplate
+    player: tuple[int, int]
+    professors: list[tuple[int, int]]
+    classmates: list[tuple[int, int]]
+    items: dict[str, list[tuple[int, int]]]
+    score: int
+    time: int
+    bombs: int
+    life: int
+
+    def save(self):
+        write(self.__dict__)
+
+def load() -> SaveData:
+    return SaveData(**read())
+
+def main():
+    save = SaveData(mazes[0], (1, 1), [(3, 4), (5, 8)], [(9, 9)], [("life", (8, 8))], 200, 100, 2, 5)
+    save.save()
+
+    del save
+
+    save = load()
+    return save
+
+if __name__ == "__main__":
+    main()
